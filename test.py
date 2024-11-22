b74bb1061c6a4bcd9fe06ff10e8e05081c078cfc
Techin-tnk <67319010028@tnk.ac.th>
2024-11-22 12:44:19 +0700

    techin

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
-print("hello python")
-print("Techin Sriputh")
-print("Sprite")
-print("18Y")
-print("07")
-print("67319010028")
-print("เล่นเกม ฟังเพลง")
+from time import sleep
+i = 0
+while True:
+    i += 1
+    print(i)
+    print("hello python")
+    print("Techin Sriputh")
+    print("Sprite")
+    print("18Y")
+    print("07")
+    print("67319010028")
+    print("เล่นเกม ฟังเพลง")
+    sleep(5)
